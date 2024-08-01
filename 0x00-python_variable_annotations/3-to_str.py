f18c94e8e43399d8c4bfd234e8eb7f3f16a4b7eb
kelvinnede <kelvinnede@gmail.com>
2024-08-01 02:46:48 -0700

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that returns the string representation of a float.
+"""
+
+def to_str(n: float) -> str:
+    """
+    Return the string representation of a float number.
+
+    Args:
+        n (float): The float number.
+
+    Returns:
+        str: The string representation of the float number.
+    """
+    return str(n)
---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that sums a list of floats.
+"""
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Sum a list of float numbers and return the result.
+
+    Args:
+        input_list (List[float]): The list of float numbers.
+
+    Returns:
+        float: The sum of the float numbers in the list.
+    """
+    return sum(input_list)
